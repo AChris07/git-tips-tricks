f56683e3980fe7698750ea9563da9364ab409978
Cristiano Amici <amicicristiano@gmail.com>
2017-07-04 23:16:50 -0500

    Add empty lines

--- script.py
+++ script.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 if __name__ == '__main__':
     name = len(sys.argv) > 1 and sys.argv[1]
-    print 'Cool stuff!'
-    print 'Current time: {0}'.format(datetime.now())
-    print 'My name is: {0}'.format(name) if name else 'I don\'t know your name'
-    print os.getcwd()
-    print subprocess.check_output(['git', 'log', '--pretty=oneline'])
+    print 'Cool stuff!\n'
+    print 'Current time: {0}\n'.format(datetime.now())
+    print 'My name is: {0}\n'.format(name) if name else 'I don\'t know your name\n'
+    print '{0}\n'.format(os.getcwd())
+    print '{0}\n'.format(subprocess.check_output(['git', 'log', '--pretty=oneline']))
     raise Exception()
