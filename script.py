455709e9837eb2d2c248ddb208cc9d4b90b98918
Cristiano Amici <amicicristiano@gmail.com>
2017-07-04 23:10:46 -0500

    Print current script's directory

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
+import os
 import sys
 from datetime import datetime
 
 if __name__ == '__main__':
     name = len(sys.argv) > 1 and sys.argv[1]
-    print 'Let\'s break this!'
+    print 'Maybe not'
     print 'Current time: {0}'.format(datetime.now())
     print 'My name is: {0}'.format(name) if name else 'I don\'t know your name'
+    print os.getcwd()
     raise Exception()
