ee1c230845eb0b50e5a393652af9e1b58833a8da
Cristiano Amici <amicicristiano@gmail.com>
2017-07-05 00:30:18 -0500

    Include colors

--- script.py
+++ script.py
@@ -1,10 +1,12 @@
 import sys
 import subprocess
 from datetime import datetime
+from colorama import init, Fore
+init(autoreset=True)
 
 if __name__ == '__main__':
     name = len(sys.argv) > 1 and sys.argv[1]
     print('More changes!')
-    print('Current time: {0}'.format(datetime.now()))
-    print('My name is: {0}'.format(name) if name else 'I don\'t know your name')
-    print('Current branch is {0}'.format(subprocess.check_output(['git', 'symbolic-ref', 'HEAD', '--short']).decode('UTF-8')))
+    print('Current time: {1}{0}'.format(datetime.now(), Fore.YELLOW))
+    print('My name is: {1}{0}'.format(name, Fore.CYAN) if name else 'I don\'t know your name')
+    print('Current branch is {1}{0}'.format(subprocess.check_output(['git', 'symbolic-ref', 'HEAD', '--short']).decode('UTF-8'), Fore.RED))
