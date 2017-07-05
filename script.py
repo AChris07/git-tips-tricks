1317aa4475349bd0595ce60f0095059b41030129
Cristiano Amici <amicicristiano@gmail.com>
2017-07-04 23:15:08 -0500

    Print Git log

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 import os
 import sys
+import subprocess
 from datetime import datetime
 
 if __name__ == '__main__':
     name = len(sys.argv) > 1 and sys.argv[1]
-    print 'Maybe not'
+    print 'Cool stuff!'
     print 'Current time: {0}'.format(datetime.now())
     print 'My name is: {0}'.format(name) if name else 'I don\'t know your name'
     print os.getcwd()
+    print subprocess.check_output(['git', 'log', '--pretty=oneline'])
     raise Exception()
