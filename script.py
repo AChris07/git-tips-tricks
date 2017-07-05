7dc6f3be0e4ec4284c1c042cdd6edfc030214404
Cristiano Amici <amicicristiano@gmail.com>
2017-07-05 00:22:12 -0500

    Convert to Python 3

--- script.py
+++ script.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 if __name__ == '__main__':
     name = len(sys.argv) > 1 and sys.argv[1]
-    print 'More changes!'
-    print 'Current time: {0}'.format(datetime.now())
-    print 'My name is: {0}'.format(name) if name else 'I don\'t know your name'
+    print('More changes!')
+    print('Current time: {0}'.format(datetime.now()))
+    print('My name is: {0}'.format(name) if name else 'I don\'t know your name')
