d3d8eaedf644a09f7e5a03861f8cb89f63b5c909
Cristiano Amici <amicicristiano@gmail.com>
2017-07-04 22:58:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Just a test repository for Admios' June ALTalk about Tips and Tricks for Git
---
+++ script.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+if __name__ == '__main__':
+    print 'First commit!'
+    print 'Current time: {0}'.format(datetime.now())
