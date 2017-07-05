61bc7301cf2823b98cfe5789630ae65d07c2bf65
Cristiano Amici <amicicristiano@gmail.com>
2017-07-04 23:02:28 -0500

    Include your name to output

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
+import sys
 from datetime import datetime
 
 if __name__ == '__main__':
-    print 'First commit!'
+    name = len(sys.argv) > 1 and sys.argv[1]
+    print 'More changes!'
     print 'Current time: {0}'.format(datetime.now())
+    print 'My name is: {0}'.format(name) if name else 'I don\'t know your name'
